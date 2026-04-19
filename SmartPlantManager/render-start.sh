725a14db8d27b89664267891363a83758ae9c114
Pandanna <chiara.mameli02@gmail.com>
2026-04-19 18:58:45 +0200

    fix

--- SmartPlantManager/render-start.sh
+++ SmartPlantManager/render-start.sh
@@ -4,7 +4,7 @@
 echo "==> Migrazione database..."
 python manage.py migrate --noinput
 
-# Crea il superuser (le variabili DJANGO_SUPERUSER_... devono essere su Render)
+# Crea il superuser
 echo "==> Creazione superuser..."
 python manage.py createsuperuser --no-input || echo "Superuser già esistente o errore saltato."
 
